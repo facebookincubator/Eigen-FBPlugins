56d1fad68201c74d8d8e9e34c453c25320283aa1
Yury Gitman <ygitman@fb.com>
2022-03-17 16:43:24 -0700

    Fix broken unit and perf tests
    Differential Revision: D34977519
    
    fbshipit-source-id: fa7d4f41327cca5807139145c4d61e3c2870f173

--- functors/IO.h
+++ functors/IO.h
@@ -2,7 +2,7 @@
 
 }  // namespace internal
 
-template<typename T> typename internal::enable_if<T::RowsAtCompileTime != Dynamic && T::ColsAtCompileTime == 1, std::ostream&>::type
+template<typename T> typename std::enable_if_t<T::RowsAtCompileTime != Dynamic && T::ColsAtCompileTime == 1, std::ostream&>
   operator <<(std::ostream& os, const ArrayBase<T>& m) { std::ostringstream ss; ss.copyfmt(os); ss.width(0); ss << '['<< m.transpose() <<']'; return (os << ss.str()); }
 
 namespace internal {

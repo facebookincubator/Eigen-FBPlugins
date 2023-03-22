b3a17800e109c2912c9c10773b5ba96816cc3eda
Yury Gitman <ygitman@meta.com>
2023-03-22 12:55:35 -0700

    Fix compilation failure with g++ on ubuntu/windows (#5)
    Summary:
    * Fix compilation issue with dimwise segments
    * Force static runtime when using g++
    
    Pull Request resolved: https://github.com/facebookincubator/Eigen-FBPlugins/pull/5
    
    Differential Revision: D44238887
    
    fbshipit-source-id: 18cc761b6bafad7b4474ce6fcc0bd6cafce6f807

--- vectorwiseop/Block.h
+++ vectorwiseop/Block.h
@@ -1,20 +1,27 @@
 /* Copyright (c) Facebook, Inc. and its affiliates. */
 
-#include "./Block-helper.h"
+template <int Size = Dynamic>
+auto segment(Index start, std::enable_if_t<(Size > 0 || Size == Dynamic) && Direction == Horizontal, Index> size = Size) const
+  { return this->_expression().template middleCols<Size>(start, size); }
 
-template <int Size = Dynamic> auto segment(Index start, Index size = Size) const
-  { return segmentHelper<Direction>::template apply<Size>(this->_expression(), start, size); }
+template <int Size = Dynamic>
+auto segment(Index start, std::enable_if_t<(Size > 0 || Size == Dynamic) && Direction == Horizontal, Index> size = Size)
+  { return this->_expression().template middleCols<Size>(start, size); }
 
-template <int Size = Dynamic> auto segment(Index start, Index size = Size)
-  { return segmentHelper<Direction>::template apply<Size>(this->_expression(), start, size); }
+template <int Size = Dynamic>
+auto segment(Index start, std::enable_if_t<(Size > 0 || Size == Dynamic) && Direction == Vertical, Index> size = Size) const
+  { return this->_expression().template middleRows<Size>(start, size); }
+
+template <int Size = Dynamic>
+auto segment(Index start, std::enable_if_t<(Size > 0 || Size == Dynamic) && Direction == Vertical, Index> size = Size)
+  { return this->_expression().template middleRows<Size>(start, size); }
+
 
 template <int Size = Dynamic> auto tail(Index size = Size) const
-  { return segment<Size>(Direction == Horizontal ? this->_expression().cols() - size
-                         : this->_expression().rows() - size, size); }
+  { return segment<Size>(Direction == Horizontal ? this->_expression().cols() - size : this->_expression().rows() - size, size); }
 
 template <int Size = Dynamic> auto tail(Index size = Size)
-  { return segment<Size>(Direction == Horizontal ? this->_expression().cols() - size
-                         : this->_expression().rows() - size, size); }
+  { return segment<Size>(Direction == Horizontal ? this->_expression().cols() - size : this->_expression().rows() - size, size); }
 
 template <int Size = Dynamic> auto head(Index size = Size) const
   { return segment<Size>(0, size); }
